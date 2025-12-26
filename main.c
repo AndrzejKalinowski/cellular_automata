af76a4354b9511c3c5001903d17254515c49b1b9
Andrzej Kalinowski <jekrekk75@gmail.com>
2025-12-26 03:05:11 +0100

    Basic working game of life. Lots more to do. For now it displays, the rules work, not much else.  the test pattern (blinker) is hard coded (for now ofc)

--- main.c
+++ main.c
@@ -1,107 +1,124 @@
 // Andrzej Kalinowski 2025
+// Implementation of the classic Conway's Game of Life
+// using C and SDL2 liblary
 
+// Basic C includes
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 
+// SDL2 include
 #include <SDL2/SDL.h>
 
-// Define MAX and MIN macros
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+// Some defines
+#define GOL_WIDTH 120  // how many columns of cells
+#define GOL_HEIGHT 120  // how many rows of cells
 
-// Define screen dimensions
-#define SCREEN_WIDTH    800
-#define SCREEN_HEIGHT   600
+#define CELL_SIZE 5
+
+#define SCREEN_WIDTH    GOL_WIDTH*CELL_SIZE     // in pixels
+#define SCREEN_HEIGHT   GOL_HEIGHT*CELL_SIZE    // in pixels
+
+// A function that counts how many neighbors does a cell at (x, y) have
+int count_alive_neighbors(int cell_array[GOL_WIDTH][GOL_HEIGHT], int x, int y){
+    int count = 0;
+    if(cell_array[x - 1][y - 1] == 1)
+        count++;
+    if(cell_array[x - 1][y] == 1)
+        count++;
+    if(cell_array[x - 1][y + 1] == 1)
+        count++;
+    if(cell_array[x][y - 1] == 1)
+        count++;
+    if(cell_array[x][y + 1] == 1)
+        count++;
+    if(cell_array[x + 1][y - 1] == 1)
+        count++;
+    if(cell_array[x + 1][y] == 1)
+        count++;
+    if(cell_array[x + 1][y + 1] == 1)
+        count++;
+    return count;
+}
 
 int main(int argc, char* argv[]){
-
-    printf("hello world \n");
     (void) argc;
     (void) argv;
 
-    // Initialize SDL
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not be initialized!\n"
-               "SDL_Error: %s\n", SDL_GetError());
-        return 0;
-    }
+    // Initializing SDL2
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("cellular automata", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Event e;
 
-    // Create window
-    SDL_Window *window = SDL_CreateWindow("Basic C SDL project",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SCREEN_WIDTH, SCREEN_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
-    if(!window)
-    {
-        printf("Window could not be created!\n"
-               "SDL_Error: %s\n", SDL_GetError());
+    // Initializing an array containing cell states
+    int cell_states[GOL_WIDTH][GOL_HEIGHT];
+    int cell_states_NEW[GOL_WIDTH][GOL_HEIGHT];
+    // Filling the arrays with zeros (at the begining all cells are dead)
+    for(int i = 0; i < GOL_WIDTH; i++){
+        for(int j = 0; j < GOL_HEIGHT; j++){
+            cell_states[i][j] = 0;
+            cell_states_NEW[i][i] = 0;
+        }   
     }
-    else
-    {
-        // Create renderer
-        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if(!renderer)
-        {
-            printf("Renderer could not be created!\n"
-                   "SDL_Error: %s\n", SDL_GetError());
+    // cell_states[50][50] = 1;    // for testing
+    // A simple blinker is defined:
+    cell_states[60][30] = 1;    // for testing
+    cell_states[60][31] = 1;    // for testing
+    cell_states[60][32] = 1;    // for testing
+
+    int quit = 0;
+    while(!quit){
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_QUIT){
+                quit = 1;
+            }    
         }
-        else
-        {
-            // Declare rect of square
-            SDL_Rect squareRect;
+        // Clearing the screen with a dark gray background
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x05, 0x05, 0xFF);
+        SDL_RenderClear(renderer);
+        
+        SDL_SetRenderDrawColor(renderer, 0x50, 0xa0, 0xee, 0xff);   // alive cell color
+        SDL_Rect cell_square;
+        cell_square.w = CELL_SIZE;
+        cell_square.h = CELL_SIZE;
 
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-            // Event loop exit flag
-            bool quit = false;
-
-            // Event loop
-            while(!quit)
-            {
-                SDL_Event e;
-
-                // Wait indefinitely for the next available event
-                SDL_WaitEvent(&e);
-
-                // User requests quit
-                if(e.type == SDL_QUIT)
-                {
-                    quit = true;
+        // Drawing the cells, according to their state
+        for(int i = 0; i < GOL_WIDTH; i++){
+            for(int j = 0; j < GOL_HEIGHT; j++){
+                if(cell_states[i][j]){
+                    cell_square.x = i*CELL_SIZE;
+                    cell_square.y = j*CELL_SIZE;
+                    SDL_RenderFillRect(renderer, &cell_square);
                 }
-
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                // Clear screen
-                SDL_RenderClear(renderer);
-
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0x20, 0x05, 0x00, 0x00);
-
-                // Draw filled square
-                SDL_RenderFillRect(renderer, &squareRect);
-
-                // Update screen
-                SDL_RenderPresent(renderer);
             }
-
-            // Destroy renderer
-            SDL_DestroyRenderer(renderer);
         }
-
-        // Destroy window
-        SDL_DestroyWindow(window);
+        // Updating cell states according to the rules of gol
+        for(int i = 0; i < GOL_WIDTH; i++){
+            for(int j = 0; j < GOL_HEIGHT; j++){
+                if(count_alive_neighbors(cell_states, i, j) == 3){
+                    cell_states_NEW[i][j] = 1;
+                }
+                else if(cell_states[i][j] == 1 && count_alive_neighbors(cell_states, i, j) == 2){
+                    cell_states_NEW[i][j] = 1;
+                }
+                else{
+                    cell_states_NEW[i][j] = 0;
+                }
+            }
+        }
+        // Coping the contents of "new" array to the main one
+        for(int i = 0; i < GOL_WIDTH; i++){
+            for(int j = 0; j < GOL_HEIGHT; j++){
+                cell_states[i][j] = cell_states_NEW[i][j];
+            }
+        }
+        SDL_RenderPresent(renderer);    // Updating screen
+        SDL_Delay(200);
     }
-    // Quit SDL
-    SDL_Quit();
 
+    // Quitting
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
